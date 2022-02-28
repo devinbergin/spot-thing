9232ef705f474f1b4d6e43fd2d8e520dfa6b9342
devinbergin <63432374+devinbergin@users.noreply.github.com>
2022-02-28 17:36:01 -0500

    framework setup
    -setting up the frame and adding placeholder album art

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 910,
+    height: 300,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('small-spot.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished

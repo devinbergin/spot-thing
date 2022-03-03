1fbfbff6de6574e5739384f06d061ea6a4bdb5a3
devinbergin <63432374+devinbergin@users.noreply.github.com>
2022-03-03 12:02:34 -0500

    Min Func!!!
    the registration is still manual, but there's minimum function with the API! It can move to the next song, fetch now playing title and refresh the access token .
    
    also added jquery

--- preload.js
+++ preload.js
@@ -1,82 +1,111 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
+  	const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
-  }
+}
+	
+	// jquery
+	var $ = require('jquery');
+	window.jQuery = window.$ = $;
 
+	// spotify api
 	var SpotifyWebApi = require('spotify-web-api-node');
 
-	/**
-	 * This example uses the Client Credentials authorization flow. 
-	 */
-
-	/**
-	 * Get the credentials from Spotify's Dashboard page.
-	 * https://developer.spotify.com/dashboard/applications
-	 */
-
-	 var scopes = ['user-read-private', 'user-read-email'],
-	 	redirectUri = 'http://localhost:8888/callback',
-	 	clientId = 'cda49a979d894afaaa13b9975b773cf9',
+	var scopes = ['user-read-currently-playing','user-read-playback-position','user-modify-playback-state','user-read-playback-state'],
+		redirectUri = 'http://localhost:8888/callback',
+		clientId = 'cda49a979d894afaaa13b9975b773cf9',
 		clientSecret = 'd88c8c755c3d471c8c7de6db709c21df',
-	 	state = 'user-read-playback-state';
-   
+		state = '';
+
 	// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
 	var spotifyApi = new SpotifyWebApi({
 		redirectUri: redirectUri,
 		clientId: clientId
 	});
-   
-   	// Create the authorization URL
-   	var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
-   
-   	// https://accounts.spotify.com/authorize?client_id=cda49a979d894afaaa13b9975b773cf9&response_type=code&redirect_uri=http://localhost:8888/callback&scope=user-read-private%20user-read-email&state=user-read-playback-state
-   	console.log(authorizeURL);
 
-   	// URL after I approved access
-   	// http://localhost:8888/callback?code=AQCUuT1p9E2VaHgzdFBrBvsEStiaxt_P7uWlhEeUuT8kLLTFoy50vAzdGExrkNsJ5Rikq7O46UhEGm9x5hpwWGybBjx5xQo0DcBu10BkYiS3npGKf70ibVFKinebNiu62Zgz4nBj0Kam44ZSQidzCZvr0HD5KLLNkuvqNnVzfru9SAeH7mC82wii-mvkJFQtoixSQ7KQNugSIaDe2NGLhVNDAzjiiQ&state=user-read-playback-state
+	// Create the authorization URL
+	var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+	// 1. Open the app and get this URL from console.
+	// 2. visit the url in browser and copy the code only into the code var below inside spotLogo func	
+	// 3. Refresh the app ctrl+shift+r click the spot logo
+	// 4. USE THE APP BABY
+	console.log(authorizeURL);
 
 	var credentials = {
 	clientId: 'cda49a979d894afaaa13b9975b773cf9',
 	clientSecret: 'd88c8c755c3d471c8c7de6db709c21df',
 	redirectUri: 'http://localhost:8888/callback'
 	};
+		
+	var spotifyApi;
 	
-	var spotifyApi = new SpotifyWebApi(credentials);
+	// 'Registration' button atm
+	$("#spotLogo").click(function() {
+		spotifyApi = new SpotifyWebApi(credentials);
+		
+		// The code that's returned as a query parameter to the redirect URI
+		var code = 'AQAjqAhwfXNJUjBPxL2tJifZlcBnw3VHkfAWo0aUglDHYMA6C0rOjxcX899A22-I03bjnT3Wle2ZM8amroJlA6VU6ISVut4N0TvcRxmkXtfzSIKQWiOToxImIZzd-9Utnf-ulr8dWgvloEF2Z-l9_xuENY2SGKTk2yzkcGTrv3tapNq1jQSs4-qaoyhM8lW7A4Xgbla_Zzm57WbLD4Uy-2k2r80EuXBfaR-qr6fInlohlbIUKOi_lpFHlgs32j3SiQpt9J6m1KtmfGoLiD9ncSu0GnJLMRkirL2FrzqfyDLmn8xJ3U-5f7zVsz4S7EZO3uY';
 	
-	// The code that's returned as a query parameter to the redirect URI
-	var code = 'AQC8y5DFI218K8y4NWwQI2JGSVgHVOSe219AR2xVZ-VndQ1LZYcWjEtkLHj2k5PINtQfh-waMBuCA78XlWxhuZtOtAT_lYAgajgbk5YUgAL0wtYo68kTemGIvrSfnpuE3jklWvLPgLpVsHj9BGHJUpFl_XMEUn9RIOYlKD2CvK2gB_T_Ql0bIC6STwBaEHYbZyn8dSIr-UTw3D2xq3u-rav9Tr4uCA';
-	
-	// Retrieve an access token and a refresh token
-	spotifyApi.authorizationCodeGrant(code).then(
-	function(data) {
-		console.log('The token expires in ' + data.body['expires_in']);
-		console.log('The access token is ' + data.body['access_token']);
-		console.log('The refresh token is ' + data.body['refresh_token']);
-	
-		// Set the access token on the API object to use it in later calls
-		spotifyApi.setAccessToken(data.body['access_token']);
-		spotifyApi.setRefreshToken(data.body['refresh_token']);
-	},
-	function(err) {
-		console.log('Something went wrong!', err);
-	}
-	);
-
-	// Set the access token pulled above
-	//spotifyApi.setAccessToken('BQAuj7GiXXo3jWJcTsHAeq5vVOkg4xq9w-3ziWC3wIqcAYS2NwtfcfmXERVyKQFOfodoSXCn170YRTZQZ2-s6Gb8TqwYZsdTMLklfsZ_JnBAsfUvx7tpChvDxvQ10iW594LDP4Yq4QBjE0va4gc5zprAZw3XwSw');
-
-	/* Get Elvis' albums
-	spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+		// Retrieve an access token and a refresh token
+		spotifyApi.authorizationCodeGrant(code).then(
 		function(data) {
-			console.log('Artist albums', data.body);
+			console.log('The token expires in ' + data.body['expires_in']);
+			console.log('The access token is ' + data.body['access_token']);
+			console.log('The refresh token is ' + data.body['refresh_token']);
+		
+			// Set the access token on the API object to use it in later calls
+			spotifyApi.setAccessToken(data.body['access_token']);
+			spotifyApi.setRefreshToken(data.body['refresh_token']);
 		},
 		function(err) {
-			console.error(err);
+			console.log('Something went wrong!', err);
 		}
-	);*/
+		);
+	});
+
+	// Need to move thise somewhere else.. might also need to dynamically do it whenever the app gets a response back that the token has expired
+	$("#shuffle").click(function() {
+		// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+		spotifyApi.refreshAccessToken().then(
+			function(data) {
+			console.log('The access token has been refreshed!');
+		
+			// Save the access token so that it's used in future calls
+			spotifyApi.setAccessToken(data.body['access_token']);
+			},
+			function(err) {
+			console.log('Could not refresh access token', err);
+			}
+		);
+	});
+	
+	// TODO switch this over to actually pausing the song
+	$("#pause").click(function() {
+		// Get the User's Currently Playing Track 
+		spotifyApi.getMyCurrentPlayingTrack()
+			.then(function(data) {
+				console.log('Now playing: ' + data.body.item.name);
+			}, function(err) {
+				console.log('Something went wrong!', err);
+		});
+	});
+
+	// TODO: when you click next, get the currently playing info and update it in the UI
+	// Skip to the next track
+	$("#nextSong").click(function() {
+		// Skip User’s Playback To Next Track
+		spotifyApi.skipToNext()
+			.then(function() {
+				console.log('Skip to next');
+			}, function(err) {
+				//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
+				console.log('Something went wrong!', err);
+			});
+	});
+	
 
   
 })

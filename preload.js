378b72dabb90665efdd55f2228de04b1c13786c9
devinbergin <63432374+devinbergin@users.noreply.github.com>
2022-03-04 08:33:20 -0500

    form 1 progress

--- preload.js
+++ preload.js
@@ -5,35 +5,64 @@ window.addEventListener('DOMContentLoaded', () => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
 }
-	
+
+	// Set to 1 to turn on console logging
+	var debug = 1;
+
 	// jquery
 	var $ = require('jquery');
 	window.jQuery = window.$ = $;
 
 	// spotify api
 	var SpotifyWebApi = require('spotify-web-api-node');
+	
+	// Declare user input variables
+	var clientID;
+	var clientSecret;
+	var redirectURI;
+	var callbackURL;
 
-	var scopes = ['user-read-currently-playing','user-read-playback-position','user-modify-playback-state','user-read-playback-state'],
-		redirectUri = 'http://localhost:8888/callback',
-		clientId = 'cda49a979d894afaaa13b9975b773cf9',
-		clientSecret = 'd88c8c755c3d471c8c7de6db709c21df',
-		state = '';
+	// TODO: Need to add some error checking for the forms not being empty
+	// first form
+	$("#idSecretNext").click(function() {	
+		// we have the secrets
+		clientID = $('#clientIDInput').val();
+		clientSecret = $('#clientSecretInput').val();
+		redirectURI = $('#redirectInput').val();
 
-	// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-	var spotifyApi = new SpotifyWebApi({
-		redirectUri: redirectUri,
-		clientId: clientId
+		debug && console.log(clientID);
+		debug && console.log(clientSecret);
+		debug && console.log(redirectURI);
+
+
+		var scopes = ['user-read-currently-playing','user-read-playback-position','user-modify-playback-state','user-read-playback-state'],
+			redirectUri = 'http://localhost:8888/callback',
+			clientId = 'cda49a979d894afaaa13b9975b773cf9',
+			clientSecret = 'd88c8c755c3d471c8c7de6db709c21df',
+			state = '';
+
+		// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+		var spotifyApi = new SpotifyWebApi({
+			redirectUri: redirectUri,
+			clientId: clientId
+		});
+
+		// Create the authorization URL
+		var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+		debug && console.log(authorizeURL);
+
+		// Open the authorize URL for the user to accept and copy the callback url
+		window.open(authorizeURL, '_blank', 'location=yes,height=900,width=700,scrollbars=no,status=yes');
+
+		// Problems with this atm - there's no URL bar for the user to copy from
 	});
-
-	// Create the authorization URL
-	var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
 
 	// 1. Open the app and get this URL from console.
 	// 2. visit the url in browser and copy the code only into the code var below inside spotLogo func	
 	// 3. Refresh the app ctrl+shift+r click the spot logo
 	// 4. Click the spotify logo
 	// 5. USE THE APP BABY
-	console.log(authorizeURL);
 
 	var credentials = {
 	clientId: 'cda49a979d894afaaa13b9975b773cf9',
@@ -53,16 +82,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		// Retrieve an access token and a refresh token
 		spotifyApi.authorizationCodeGrant(code).then(
 		function(data) {
-			console.log('The token expires in ' + data.body['expires_in']);
-			console.log('The access token is ' + data.body['access_token']);
-			console.log('The refresh token is ' + data.body['refresh_token']);
+			debug && console.log('The token expires in ' + data.body['expires_in']);
+			debug && console.log('The access token is ' + data.body['access_token']);
+			debug && console.log('The refresh token is ' + data.body['refresh_token']);
 		
 			// Set the access token on the API object to use it in later calls
 			spotifyApi.setAccessToken(data.body['access_token']);
 			spotifyApi.setRefreshToken(data.body['refresh_token']);
 		},
 		function(err) {
-			console.log('Something went wrong!', err);
+			debug && console.log('Something went wrong!', err);
 		}
 		);
 	});
@@ -72,13 +101,13 @@ window.addEventListener('DOMContentLoaded', () => {
 		// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
 		spotifyApi.refreshAccessToken().then(
 			function(data) {
-			console.log('The access token has been refreshed!');
+				debug && console.log('The access token has been refreshed!');
 		
 			// Save the access token so that it's used in future calls
 			spotifyApi.setAccessToken(data.body['access_token']);
 			},
 			function(err) {
-			console.log('Could not refresh access token', err);
+				debug && console.log('Could not refresh access token', err);
 			}
 		);
 	});
@@ -88,12 +117,12 @@ window.addEventListener('DOMContentLoaded', () => {
 		// Get the User's Currently Playing Track 
 		spotifyApi.getMyCurrentPlayingTrack()
 			.then(function(data) {
-				console.log('Now playing track: ' + data.body.item.name);
-				console.log('Now playing artist[0]: ' + data.body.item.artists[0].name);
-				console.log('Now playing album: ' + data.body.item.album.name);
-				console.log('Now playing image: ' + data.body.item.album.images[0].url);
-				console.log('Now playing song progress ms: ' + data.body.progress_ms);
-				console.log('Now playing song duration ms: ' + data.body.item.duration_ms);
+				debug && console.log('Now playing track: ' + data.body.item.name);
+				debug && console.log('Now playing artist[0]: ' + data.body.item.artists[0].name);
+				debug && console.log('Now playing album: ' + data.body.item.album.name);
+				debug && console.log('Now playing image: ' + data.body.item.album.images[0].url);
+				debug && console.log('Now playing song progress ms: ' + data.body.progress_ms);
+				debug && console.log('Now playing song duration ms: ' + data.body.item.duration_ms);
 				//console.log('Now playing: ' + JSON.stringify(data.body));
 
 				var percentComplete = (data.body.progress_ms/data.body.item.duration_ms)*100;
@@ -106,7 +135,7 @@ window.addEventListener('DOMContentLoaded', () => {
 				$('.progress-bar').css('width', percentComplete+'%').attr('aria-valuenow', percentComplete);
 
 			}, function(err) {
-				console.log('Something went wrong!', err);
+				debug && console.log('Something went wrong!', err);
 		});
 	  }, 1000);
 
@@ -120,23 +149,23 @@ window.addEventListener('DOMContentLoaded', () => {
 		
 			spotifyApi.setShuffle(true)
 			.then(function() {
-				console.log('Shuffle is on.');
+				debug && console.log('Shuffle is on.');
 				$('#shuffle').addClass('shuffleActive');
 				$('#shuffle').removeClass('shuffleInactive');
 			}, function  (err) {
 				//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-				console.log('Something went wrong!', err);
+				debug && console.log('Something went wrong!', err);
 			});
 
 		} else {
 			spotifyApi.setShuffle(false)
 			.then(function() {
-				console.log('Shuffle is off.');
+				debug && console.log('Shuffle is off.');
 				$('#shuffle').removeClass('shuffleActive');
 				$('#shuffle').addClass('shuffleInactive');
 			}, function  (err) {
 				//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-				console.log('Something went wrong!', err);
+				debug && console.log('Something went wrong!', err);
 			});
 		}
 	});
@@ -145,11 +174,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		// Pause the User's Playback 
 		spotifyApi.pause()
 		.then(function() {
-			console.log('Playback paused');
+			debug && console.log('Playback paused');
 			$('#playPause').html('<i class="fa-solid fa-play" id="play"></i>');
 		}, function(err) {
 			//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-			console.log('Something went wrong!', err);
+			debug && console.log('Something went wrong!', err);
 		});
 	});
 
@@ -157,11 +186,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		// Start/Resume a User's Playback 
 		spotifyApi.play()
 		.then(function() {
-			console.log('Playback started');
+			debug && console.log('Playback started');
 			$('#playPause').html('<i class="fa-solid fa-pause" id="pause"></i>');
 		}, function(err) {
 			//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-			console.log('Something went wrong!', err);
+			debug && console.log('Something went wrong!', err);
 		});
 	});
 
@@ -171,10 +200,10 @@ window.addEventListener('DOMContentLoaded', () => {
 		// Skip User’s Playback To Next Track
 		spotifyApi.skipToNext()
 		.then(function() {
-			console.log('Skip to next');
+			debug && console.log('Skip to next');
 		}, function(err) {
 			//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-			console.log('Something went wrong!', err);
+			debug && console.log('Something went wrong!', err);
 		});
 	});
 
@@ -184,10 +213,10 @@ window.addEventListener('DOMContentLoaded', () => {
 		// Skip User’s Playback To Previous Track 
 		spotifyApi.skipToPrevious()
 		.then(function() {
-			console.log('Skip to previous');
+			debug && console.log('Skip to previous');
 		}, function(err) {
 			//if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-			console.log('Something went wrong!', err);
+			debug && console.log('Something went wrong!', err);
 		});
 	});
 	

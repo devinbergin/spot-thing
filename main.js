8a89ab475561bb09f391a648c2daad60c3aa0406
Devin Bergin <63432374+devinbergin@users.noreply.github.com>
2023-09-15 11:47:44 -0400

    Version bump to 1.0.3

--- main.js
+++ main.js
@@ -58,4 +58,3 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
